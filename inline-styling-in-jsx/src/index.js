e6cee9cb1884ff235359c80538a6fde504c9db1a
Yulian Belov <ybelo@softserveinc.com>
2023-12-07 12:59:48 +0200

    commit

--- inline-styling-in-jsx/src/index.js
+++ inline-styling-in-jsx/src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
+const time = new Date().getHours();
+
+const customStyle = {
+  color: "crimson",
+};
+
+var greating = "";
+
+if (time< 12) {
+  greating = "Good Morning";
+  customStyle.color = "red";
+} else if (time >= 18) {
+  greating = "Good Evening";
+  customStyle.color = "blue";
+} else {
+  greating = "Good Afternoon";
+  customStyle.color = "green";
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <h1>Hello</h1>
+    <h1 style={customStyle}>{greating}</h1>
 );
