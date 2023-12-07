ae16cc345fc261e88d834d57578383479447a508
Yulian Belov <ybelo@softserveinc.com>
2023-12-07 11:33:50 +0200

    commit

--- jsx-code-challenge/src/index.js
+++ jsx-code-challenge/src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const name = "Bob";
+const number = Math.floor(Math.random() * 10);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
-    <h1>Hello React</h1>
-    <ul>
-      <li>Apple</li>
-      <li>Mango</li>
-      <li>Orange</li>
-    </ul>
+    <h1>Hello {name}</h1>
+    <h2>Your lucky number is {number}</h2>
+    <p>© {new Date().getFullYear()}</p>
   </div>
 );
