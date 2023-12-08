0be4ac9c3af58b587dc5f3ba337d07ab0ebdcb9b
Yulian Belov <ybelo@softserveinc.com>
2023-12-08 17:28:27 +0200

    commit

---
+++ react-playground/src/calculator.js
@@ -0,0 +1,17 @@
+function add(a, b) {
+  return a + b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function substract(a, b) {
+  return a - b;
+}
+
+function devide(a, b) {
+  return a / b;
+}
+
+export { add, multiply, substract, devide };
--- react-playground/src/index.js
+++ react-playground/src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
+import * as Calculator from "./calculator";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <div>
     <App />
+    <ul>
+      <li>{Calculator.add(4, 4)}</li>
+      <li>{Calculator.multiply(4, 4)}</li>
+      <li>{Calculator.substract(4, 4)}</li>
+      <li>{Calculator.devide(4, 4)}</li>
+    </ul>
   </div>
 );
