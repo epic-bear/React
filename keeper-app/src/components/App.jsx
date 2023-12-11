90b35b72bcd85b3981eeb4eb25507bf465cd6b54
Yulian Belov <ybelo@softserveinc.com>
2023-12-11 17:38:26 +0200

    commit

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -5,15 +5,18 @@ import Note from "./Note";
 import Footer from "./Footer";
 
 var notes = [
-    {
+    { 
+        id: 1,
         title: "title1",
         content: "text"
     },
-    {
+    {   
+        id: 2,
         title: "title2",
         content: "text"
     },
-    {
+    {   
+        id: 3,
         title: "title3",
         content: "text"
     }
