85f9e0e5bba40ce3ec001a228f4deb8d7b7ccb26
Yulian Belov <ybelo@softserveinc.com>
2023-12-16 11:42:22 +0200

    commit

--- keeper-app/src/components/App.jsx
+++ keeper-app/src/components/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Header";
 import Note from "./Note";
@@ -6,13 +6,23 @@ import Footer from "./Footer";
 import notes from "../notes";
 
 function createNote(note) {
-   return <Note key={note.id} title={note.title} content={note.content}/>
+  return <Note key={note.id} title={note.title} content={note.content} />;
 }
 
 function App() {
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  function getTime() {
+    setTime(new Date().toLocaleTimeString());
+  }
+
   return (
     <div>
       <Header />
+      <div>
+        <h1>{time}</h1>
+        <button onClick={getTime}>Get Time</button>
+      </div>
       {notes.map(createNote)}
       <Footer />
     </div>
