586c329a98aacf74499640b611095f60978e5b17
Yulian Belov <ybelo@softserveinc.com>
2023-12-16 13:21:39 +0200

    commit

--- keeper-app/src/components/Note.jsx
+++ keeper-app/src/components/Note.jsx
@@ -1,8 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Note(props) {
+const [isMouseOver, setMouseOver] = useState(false);
+
+function handleMouseOver() {
+  setMouseOver(true);
+}
+
+function handleMouseOut() {
+  setMouseOver(false);
+}
+
   return (
-    <div className="note">
+    <div 
+    style={{backgroundColor: isMouseOver ? "orange" : "white"}} className="note"
+    onMouseOver={handleMouseOver}
+    onMouseOut={handleMouseOut}
+    >
       <h2>{props.title}</h2>
       <p>{props.content}</p>
     </div>
